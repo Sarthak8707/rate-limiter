1130b7bc47bb143a8516c1be8954ea0cbf177c67
sarthak <sarthakssingh49@gmail.com>
2026-02-24 16:25:48 +0530

    feat(rate-limit): Sliding Window for every id

--- src/rateLimit.js
+++ src/rateLimit.js
@@ -1,33 +1,28 @@
 
-const map = new Map();
-
-const checkForLimit = (userId) => {
-
-    const {reqCount, currentTime} = map.get(userId);
-    
-    const timeDiff = Date.now() - currentTime;
-    
-    if(timeDiff > 60000) reqCount = 1, currentTime = Date.now();
-    else reqCount++; 
-
-    if(reqCount > 1000) return false;
-
-    return true;
-}
+const requestLog = new Map(); // store (id, array of timestamps)
+const WINDOW_SIZE = 60*1000;  
+const MAX_REQUESTS = 1000;
 
 export const rateLimiter = (req, res, next) => {
+    
     const id = req.user.id;
+    let timeStamps = requestLog.get(id) || [];
+    const now = Date.now();
 
-    // if request is made first time
+    // remove timestamps older than a minute
 
-    if(!map.has(id)) {
-        map.set(id, {reqCount: 1, currentTime: Date.now()});
-    }
+    timeStamps.filter((timeStamp) => now - timeStamp < WINDOW_SIZE);
 
-    const underLimit = checkForLimit(id);
+    // check limit
 
-    if(!underLimit) res.status(429).json({msg: "Too many requests"})
+    if(timeStamps.length >= MAX_REQUESTS) return res.status(429).json({msg: "Too many requests"});
+
+    // add to log
+    
+    timeStamps.push(now);
+
+    requestLog.set(id, timeStamps);
 
     next();
-    
+
 }
