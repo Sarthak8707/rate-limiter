edf57d67af3cf657984c258d8108c21b73b3ff0a
sarthak <sarthakssingh49@gmail.com>
2026-02-25 23:59:10 +0530

    feat(rate-limit): Sliding Window Counter

--- src/rateLimit.js
+++ src/rateLimit.js
@@ -1,28 +1,41 @@
 
-const requestLog = new Map(); // store (id, array of timestamps)
+const requestLog = new Map(); // store (id, {currentWindow, currentCount, previousCount})
 const WINDOW_SIZE = 60*1000;  
 const MAX_REQUESTS = 1000;
 
 export const rateLimiter = (req, res, next) => {
-    
-    const id = req.user.id;
-    let timeStamps = requestLog.get(id) || [];
+    const id = req.id;
     const now = Date.now();
+    const currentWindow = Math.floor(now/WINDOW_SIZE)*WINDOW_SIZE;
+    if(!requestLog.has(id)){
+        requestLog.set(id, {currentWindow: currentWindow, currentCount: 0, previousCount: 0});
+    }
+    const data = requestLog.get(id);
 
-    // remove timestamps older than a minute
+    // Not in the same window
+    if(data.currentWindow != currentWindow){
+        const windowDiff = (currentWindow-data.currentWindow)/WINDOW_SIZE;
+        if(windowDiff == 1){
+            data.previousCount = data.currentCount;
+        }
+        else{
+            data.previousCount = 0;
+        }
+        data.currentCount = 0;
+        data.currentWindow = currentWindow;
+    }
+    const timeIntoWindow = now - currentWindow;
+    const timeLeft = WINDOW_SIZE - timeIntoWindow;
+    const overlapRatio = timeLeft/WINDOW_SIZE;
+    const requests = data.currentCount + data.previousCount*overlapRatio;
 
-    timeStamps.filter((timeStamp) => now - timeStamp < WINDOW_SIZE);
+    if(requests > MAX_REQUESTS){
+        return res.status(429).json({msg: "Too manny requests"});
+    }
 
-    // check limit
-
-    if(timeStamps.length >= MAX_REQUESTS) return res.status(429).json({msg: "Too many requests"});
-
-    // add to log
-    
-    timeStamps.push(now);
-
-    requestLog.set(id, timeStamps);
-
+    data.currentCount += 1;
     next();
+
+    
 
 }
